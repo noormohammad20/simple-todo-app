298a068f9c4ad93d0dad70d81fd6c58c2ce49237
noormohammad20 <mdsaim204@gmail.com>
2022-08-22 22:46:11 +0600

    create todo reducer

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,10 @@
+export const initialState = [{
+    id: 1,
+    text: 'learn react JS',
+    completed: true,
+}, {
+    id: 2,
+    text: 'learn redux',
+    completed: false,
+    color: 'red',
+}]
---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,63 @@
+import { initialState } from "./initialState"
+import { ADDED, DELETED, ALLCOMPLETED, CLEARCOMPLETED, COLORSELECTED, TOGGLED } from "./actionTypes"
+
+function nextTodoId(todos) {
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+    return maxId + 1
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADDED:
+            return [
+                ...state,
+                {
+                    id: nextTodoId(state)
+                }
+            ]
+        case TOGGLED:
+            const { completed } = action.payload
+
+            return state.map(todo => {
+                if (todo.id !== action.payload) {
+                    return todo
+                }
+                return {
+                    ...todo,
+                    completed: !completed
+                }
+            })
+
+        case COLORSELECTED:
+            const { todoId, color } = action.payload
+
+            return state.map(todo => {
+                if (todo.id !== todoId) {
+                    return todo
+                }
+                return {
+                    ...todo,
+                    color: color
+                }
+
+            })
+
+        case DELETED:
+            return state.filter(todo => todo.id !== action.payload)
+
+        case ALLCOMPLETED:
+            return state.map(todo => {
+                return {
+                    ...todo,
+                    completed: true
+                }
+            })
+
+        case CLEARCOMPLETED:
+            return state.filter(todo => !todo.completed)
+
+        default:
+            break
+
+    }
+}
+export default reducer
