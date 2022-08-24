339c0c4357f536a6c1da7369cbe4e1e7a91be8a6
noormohammad20 <mdsaim204@gmail.com>
2022-08-24 13:37:57 +0600

    added deleted todo method

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import cancelImg from '../assets/images/cancel.png'
-import { colorSelected, deleted } from '../redux/todos/actions'
 import updateStatus from '../redux/todos/thunk/updateStatus'
 import updateColor from '../redux/todos/thunk/updateColor'
+import deleteTodo from '../redux/todos/thunk/deleteTodo'
 
 const Todo = ({ todo }) => {
     const dispatch = useDispatch()
@@ -19,14 +19,14 @@ const Todo = ({ todo }) => {
     }
 
     const handleDelete = (todoId) => {
-        dispatch(deleted(todoId))
+        dispatch(deleteTodo(todoId))
     }
     return (
         <div
             className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0"
         >
             <div
-                className={`rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${completed && ' border-green-500 focus-within:border-green-500'}`}
+                className={`relative rounded-full bg-white border-2 border-gray-400 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${completed && ' border-green-500 focus-within:border-green-500'}`}
             >
                 <input
                     type="checkbox"
